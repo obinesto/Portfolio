63ea766128970d6b4fa37973a4a9ae5a7aa5a9ed
Cyprian Obi <obicyprian180@gmail.com>
2025-08-19 19:41:08 +0100

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -77,7 +77,7 @@ export function Hero() {
           </TextEffect>
         </div>
         <Link
-          href="https://drive.google.com/file/d/1DarqyLLb3QBEOwkz858uNhA-OyxvanSu/view?usp=sharing"
+          href="https://drive.google.com/file/d/1p3b1Gue3N1jTIOtk2b-teDs3kVYLbyCf/view?usp=sharing"
           target="blank"
           className="inline-block px-6 py-2 border border-[#4ECCA3] text-[#4ECCA3] hover:bg-[#4ECCA3] hover:bg-opacity-10 transition-colors hero-anim"
         >
